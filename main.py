9c9d326a4aa91e7f9d820ec433ae721a17ee119e
Marci <matusekma@gmail.com>
2020-08-29 17:19:02 +0200

    Run training in main.py

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import math
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import torch
+from torch import nn
 
+from config import epochs, emsize, nhead, nhid, dropout, nlayers
+from data import val_data, test_data, TEXT
+from model import TransformerModel, device
+from train import train
+from evaluate import evaluate
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    ntokens = len(TEXT.vocab.stoi)  # the size of vocabulary
+    model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, dropout).to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    lr = 5.0  # learning rate
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
+
+    best_val_loss = float("inf")
+    best_model = None
+
+    for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+        train(model, optimizer, criterion, scheduler, epoch)
+        val_loss = evaluate(model, val_data, criterion)
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+              'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+                                         val_loss, math.exp(val_loss)))
+        print('-' * 89)
+
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            best_model = model
+
+        scheduler.step()
+
+    test_loss = evaluate(best_model, test_data, criterion)
+    print('=' * 89)
+    print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(
+        test_loss, math.exp(test_loss)))
+    print('=' * 89)
